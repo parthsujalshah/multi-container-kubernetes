ad92fe9aa114b28c3d129d4ef47f3bd4ee1deb87
root <root@DESKTOP-0MQSS3B.localdomain>
2022-03-01 15:41:33 +0530

    redis hmget

--- server/index.js
+++ server/index.js
@@ -60,13 +60,13 @@ app.get('/values/current', async (req, res) => {
 
 app.post('/values', async (req, res) => {
   const index = req.body.index;
+  const indexInt = parseInt(index);
 
-  if (parseInt(index) > 40) {
+  if (indexInt > 40) {
     return res.status(422).send('Index too high');
   }
-  redisClient.hset('values', index, 'Nothing yet!');
-  redisPublisher.publish('insert', index);
-  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+  redisPublisher.publish('insert', indexInt);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [indexInt]);
 
   res.send({ working: true });
 });
--- worker/index.js
+++ worker/index.js
@@ -10,11 +10,20 @@ const redisClient = redis.createClient({
 const sub = redisClient.duplicate();
 
 function fib(index) {
-    if (index < 2) return 1;
+    console.log("in worker", index)
+    if (index < 2) {
+        return 1;
+    }
     return fib(index - 1) + fib(index - 2);
 }
 
 sub.on('message', (channel, message) => {
+  redisClient.hmget('values', message, function(err, data) {
+    if (err) console.error(err);
+    if (data === null) {
+        redisClient.hset('values', message, parseInt(fib(parseInt(message))));
+    }
+  });
     redisClient.hset('values', message, fib(parseInt(message)));
 });
 
